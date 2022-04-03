fb95acba9aa761be97eb0d37d32df9a1cdcf8110
PRITIPADMA <pritipadmamishra38@gmail.com>
2022-04-03 15:14:38 +0530

    Frontend

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,12 @@ const app = express();
 
 app.use(express.json());
 
+var cors = require("cors");
+
+app.use(cors());
+
 //Route Importing
 const user = require("./routes/userRoute");
-app.use("/api/v1", user)
+app.use("/api/v1", user);
 
 module.exports = app;
--- frontend/src/components/table.js
+++ frontend/src/components/table.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./table.css";
 import { AiOutlineMenu } from "react-icons/ai";
+import axios from "axios";
 
-function table() {
+function Table() {
+  const [event, setEvent] = useState([]);
+  const getDetails = () => {
+    axios
+      .get("http://localhost:4000/api/v1/event")
+      .then((res) => {
+        setEvent(res.data.events);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <>
       <div className="header">Your Campaigns</div>
+      <div className="get__details" onClick={getDetails}>
+        Get details
+      </div>
       <div className="table">
         <table className="content-table">
           <thead>
@@ -18,29 +33,22 @@ function table() {
             </tr>
           </thead>
 
-          <tr>
-            <td>Covid Help</td>
-            <td id="status">Active</td>
-            <td>$25 out of $6000</td>
-            <td>22/02/2022</td>
-            <td className="action">
-              <AiOutlineMenu />
-            </td>
-          </tr>
-
-          <tr>
-            <td>Whatever</td>
-            <td id="status">Pending</td>
-            <td>$25 out of $6000</td>
-            <td>22/02/2022</td>
-            <td className="action">
-              <AiOutlineMenu />
-            </td>
-          </tr>
+          {event.length &&
+            event.map((data) => (
+              <tr>
+                <td>{data.category}</td>
+                <td id="status">{data.status}</td>
+                <td>$25 out of $6000</td>
+                <td>{data.deadline}</td>
+                <td className="action">
+                  <AiOutlineMenu />
+                </td>
+              </tr>
+            ))}
         </table>
       </div>
     </>
   );
 }
 
-export default table;
+export default Table;
